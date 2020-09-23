adcc847550fca7770dffa977c6be00b800a324de
Carlos-SSH <carlos.anezeh@gmail.com>
2020-09-23 12:10:03 -0500

    Create test for my_none enumerable.

--- enumerables_spec.rb
+++ enumerables_spec.rb
@@ -60,7 +60,81 @@ describe Enumerable do
     end
   end
 
+  describe '#my_any?' do
+    it 'return true if any in the array complete with condition in the block' do
+      arr = %w[ant bear cat]
+      expect(arr.my_any? { |word| word.length >= 3 }).to eql(true)
+    end
 
+    it 'return true if any in the array complete with condition in the block' do
+      arr = %w[ant bear cat]
+      expect(arr.my_any? { |word| word.length >= 4 }).to eql(true)
+    end
+
+    it 'return false if no one if the array match with the regex' do
+      arr = %w[ant bear cat]
+      expect(arr.my_any?(/d/)).to eql(false)
+    end
+
+    it 'return true if any element in the array  complete with the condition of the argument' do
+      arr = [nil, false, 99]
+      expect(arr.my_any?(Integer)).to eql(true)
+    end
+
+    it 'return true if any element in the array is different than false or nil' do
+      arr = [nil, false, 99]
+      expect(arr.my_any?).to eql(true)
+    end
+
+    it 'return false is the array is empty' do
+      arr = []
+      expect(arr.my_any?).to eql(false)
+    end
+  end
+
+  describe '#my_none?' do
+    it 'return true if no array in block' do
+      arr = %w[ant bear cat]
+      expect(arr.my_none? { |word| word.length >= 5 }).to eql(true)
+    end
+
+    it 'return true if one in the array complete with condition in the block' do
+      arr = %w[ant bear cat]
+      expect(arr.my_none? { |word| word.length >= 4 }).to eql(false)
+    end
+  end
+
+  describe '#my_none?' do
+    it 'return true if one in the array match with the regex' do
+      arr = %w[ant bear cat]
+      expect(arr.my_none?(/d/)).to eql(true)
+    end
+
+    it 'return false if no one if the array is in the class' do
+      arr = [1, 3.14, 42]
+      expect(arr.my_none?(Float)).to eql(false)
+    end
+
+    it 'return true if the array is empty' do
+      arr = []
+      expect(arr.my_none?).to eql(true)
+    end
+
+    it 'return true if the array only have a nil' do
+      arr = [nil]
+      expect(arr.my_none?).to eql(true)
+    end
+
+    it 'return true if the array only have a nil and false' do
+      arr = [nil, false]
+      expect(arr.my_none?).to eql(true)
+    end
+
+    it 'return true if one in the array is true' do
+      arr = [nil, false, true]
+      expect(arr.my_none?).to eql(false)
+    end
+  end
 
 end  
 
