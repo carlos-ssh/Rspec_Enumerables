3fe4ebba6fc8f6a2801e86319bcb298281bf6b94
Carlos-SSH <carlos.anezeh@gmail.com>
2020-09-23 12:07:35 -0500

    Create tests for my_all.

--- enumerables_spec.rb
+++ enumerables_spec.rb
@@ -13,7 +13,54 @@ describe Enumerable do
     it 'return array' do
       expect(arr.my_each { |x| x }).to eql(arr)
     end
+  end
 
-      
-  end  
-end
+  describe '#my_each_with_index' do
+    it 'return enumerator if block is not given' do
+      expect(arr.my_each_with_index.class).to eql(Enumerator)
+    end
+
+    it 'return array when a block is given ' do
+      arr = [11, 22, 31, 224, 44]
+      expect(arr.my_each_with_index do |val, index|
+               puts "index: #{index} for #{val}" if val < 30
+             end).to eql([11, 22, 31, 224, 44])
+    end
+  end
+
+ describe '#my_all?' do
+    it 'return true if all in the array complete the condition in the block' do
+      arr = %w[ant bear cat]
+      expect(arr.my_all? { |word| word.length >= 3 }).to eql(true)
+    end
+
+    it 'return true if no one in the array complete the condition in the block' do
+      arr = %w[ant bear cat]
+      expect(arr.my_all? { |word| word.length >= 4 }).to eql(false)
+    end
+
+    it 'return false if no one in the array match with the regex' do
+      arr = %w[ant bear cat]
+      expect(arr.my_all?(/t/)).to eql(false)
+    end
+
+    it 'return ture if all in the array are in the argument class ' do
+      arr = [1, 2i, 3.14]
+      expect(arr.my_all?(Numeric)).to eql(true)
+    end
+
+    it 'return false if one element in the array is nil or false ' do
+      arr = [nil, false, 99]
+      expect(arr.my_all?).to eql(false)
+    end
+
+    it 'return true if the array is empty' do
+      arr = []
+      expect(arr.my_all?).to eql(true)
+    end
+  end
+
+
+
+end  
+
