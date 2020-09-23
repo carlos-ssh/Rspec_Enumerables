e6e2560d0b7a35d34151e87f0cc3efca21e2b52a
Carlos-SSH <carlos.anezeh@gmail.com>
2020-09-23 12:04:48 -0500

    Create my_each_with_index test.

--- enumerables_spec.rb
+++ enumerables_spec.rb
@@ -9,5 +9,11 @@ describe Enumerable do
     it 'return enumerator if block not given' do
       expect(arr.my_each.class).to eql(Enumerator)
     end
-  end
+
+    it 'return array' do
+      expect(arr.my_each { |x| x }).to eql(arr)
+    end
+
+      
+  end  
 end
