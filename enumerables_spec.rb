eb54aade1529fefbb68c5812719ee66b8c07e1b4
Carlos-SSH <carlos.anezeh@gmail.com>
2020-09-23 12:01:06 -0500

    Create my_each test.

--- enumerables_spec.rb
+++ enumerables_spec.rb
@@ -1,2 +1,13 @@
 
 require_relative '../lib/enumerables'
+
+
+describe Enumerable do
+  let(:arr) { [1, 2, 3, 4] }
+
+  describe '#my_each' do
+    it 'return enumerator if block not given' do
+      expect(arr.my_each.class).to eql(Enumerator)
+    end
+  end
+end
